6d9eb3f93e26069eddcab0553c3e7dade6b332a1
SMaverick78 <Smaverick78@gmail.com>
2020-02-09 18:30:50 +0300

    cardHower1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./styles.css";
-import HoverEffect1 from "./hoverEffect1";
+import CardHower1 from "./cardHower1";
 
 export default function App() {
   return (
     <div className="App">
       <div className={"container"}>
-        <HoverEffect1 />
-        <HoverEffect1 />
-        <HoverEffect1 />
+        <CardHower1 />
+        <CardHower1 />
+        <CardHower1 />
       </div>
     </div>
   );
