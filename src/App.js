e256be4cb03026235c03aa71be30c80115520c08
SMaverick78 <Smaverick78@gmail.com>
2020-02-09 16:46:46 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+import HoverEffect1 from "./hoverEffect1";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className={"container"}>
+        <HoverEffect1 />
+        <HoverEffect1 />
+        <HoverEffect1 />
+      </div>
+    </div>
+  );
+}
